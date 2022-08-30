cbbba6137b1e233a56c07b85866a27ce2bff1fe3
Ernest Owusu Fokuo <kwesifokuo@gmail.com>
2022-08-30 15:48:25 +0000

    Updates for Currency and lower and upper case

--- src/CurrencyToWords.php
+++ src/CurrencyToWords.php
@@ -4,19 +4,32 @@ namespace Phrontlyne\Modules;
 
 class CurrencyToWords
 {
-    public function format(Int $amount)
+    public function format(String $amount, String $case = "upper", String $currency)
     {
+        $word_currency = "Ghana Cedis";
+        $word_unit = "Pesewas";
+
+        if($currency == 'USD') {
+            $word_currency = "Dollars";
+            $word_unit = "Cents";
+        } elseif($currency == 'EUR') {
+            $word_currency = "Euros";
+            $word_unit = "Cents";
+        } elseif($currency == 'GBP') {
+            $word_currency = "Pounds";
+            $word_unit = "Pence";
+        }
         $f = new \NumberFormatter("en", \NumberFormatter::SPELLOUT);
         $f->setTextAttribute(\NumberFormatter::DEFAULT_RULESET, "%spellout-numbering-verbose");
         //$number = $suminsured;
-        $numberParts = explode('.', (string) $amount);
+        $numberParts = explode('.', $amount); //(string)
         $amtInWords =  $f->format($numberParts[0]);
         if (isset($numberParts[1]) && $numberParts[1] != 00) {
-            $amtInWords .= ' Ghana Cedis ' . $f->format($numberParts[1]).' Pesewas';
+            $amtInWords .= ' '.$word_currency.' ' . $f->format($numberParts[1]).' '.$word_unit;
         } else {
-            $amtInWords .= ' Ghana Cedis';
+            $amtInWords .= ' '.$word_currency;
         }
-        $amtInWords = strtoupper($amtInWords);
+        $amtInWords = $case == 'upper' ? strtoupper($amtInWords) : ucwords($amtInWords);
         return $amtInWords;
     }
 }
