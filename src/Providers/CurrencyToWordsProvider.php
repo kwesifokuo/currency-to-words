9b65220736f248d9d100fe8e314973cd2ccfdee8
Ernest Owusu Fokuo <kwesifokuo@gmail.com>
2024-02-01 17:53:30 +0000

    Added Provider

---
+++ src/Providers/CurrencyToWordsProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace CurrencyToWords\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class CurrencyToWordsProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
